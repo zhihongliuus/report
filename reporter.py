6d92b11f76f73c8f9c1ba8127e5002e78544db90
hfgc63 <hfgc63@motorola.com>
2015-06-29 20:21:44 -0700

    Rewrite structures

--- reporter.py
+++ reporter.py
@@ -4,65 +4,134 @@ from stat import S_ISDIR, ST_CTIME, ST_MODE
 import os
 import urlparse
 
-OUTPUT = ""
+HEAD = """
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  border-collapse: collapse;
+}
 
-def walk_suites(report_dir):
+td, th {
+  border: 1px solid #999;
+  padding: 0.5rem;
+  text-align: left;
+}
+</style>
+</head>
+<body>
+<table>
+"""
+
+FOOT = """</table>
+</body>
+</html>
+"""
+
+plugins = ["plugin_load_trace_log",
+           "plugin_load_logcat_log",
+           "plugin_load_screen_shot",
+           "plugin_load_calling_log",
+           "plugin_load_error_summary_log",
+           "plugin_load_carrier_name_log"]
+
+def walk_suites(report_dir, f):
     suite_names = (os.path.join(report_dir, fn) for fn in os.listdir(report_dir))
     suite_names = ((os.stat(path), path) for path in suite_names)
-    suite_names = ((stat[ST_CTIME], path) for stat, path in suite_names if S_ISDIR(stat(ST_MODE)))
-    test_results = []
+    suite_names = ((stat[ST_CTIME], path) for stat, path in suite_names if S_ISDIR(stat[ST_MODE]))
+    output = "<thead><tr>"
+    plugins = load_plugins()
+    for plugin in plugins:
+        output += "<th class=\"{class_name}\">{plugin_name}</th>".format(class_name=globals()[plugin].__doc__, plugin_name=globals()[plugin].__doc__)
+    output += "</tr></thead>\n"
+    f.write(output)
     for c_date, suite_name in sorted(suite_names):
-        test_results.append(load_plugins(suite_name))
-    pass
+        walk_suite(suite_name, f)
 
-def walk_suite(suite_name):
+def walk_suite(suite_name, f):
+    plugins = load_plugins()
+    output = "<tbody><tr>"
     for testcase in os.listdir(suite_name):
-        load_plugins(testcase)
+        for plugin in plugins:
+            globals()[plugin](os.path.join(suite_name, testcase), f)
+    output += "</tr></tbody>\n"
+    f.write(output)
 
-def load_plugins(testcase):
-    global OUTPUT
-    for plugin in globals().keys():
-        if plugin.startswith("plugin_"):
-            OUTPUT += globals()[plugin](testcase)
+def load_plugins():
+#    return [plugin for plugin in globals().keys() if plugin.startswith("plugin_")]
+    return plugins
 
-def plugin_load_trace_log(testcase):
+def plugin_load_trace_log(testcase, f):
     """TraceLog"""
-    result = ""
+    output = "<td class={0}>\n".format("TraceLog")
     for root, dir, files in os.walk(testcase):
         for name in files:
             if name.startswith("trace_log"):
-                result += urlparse.urljoin(root, name)
-    return result
+                output += "<a href=\"{0}\">{1}</a>".format(urlparse.urljoin(root, name), name)
+    output += "</td>\n"
+    f.write(output)
 
-def plugin_load_logcat_log(testcase):
+def plugin_load_logcat_log(testcase, f):
     """Logcat"""
-    result = ""
+    output = "<td>\n"
     for root, dir, files in os.walk(testcase):
         for name in files:
             if name.startswith("logcat"):
-                result += urlparse.urljoin(root, name)
-    return result
+                output += "<a href=\"{0}\">{1}</a>".format(urlparse.urljoin(root, name), name)
+    output += "</td>\n"
+    f.write(output)
 
-def plugin_load_screen_shot(testcase):
+def plugin_load_screen_shot(testcase,f):
     """Screenshot"""
-    result = ""
+    output = "<td>\n"
     for root, dir, files in os.walk(testcase):
         for name in files:
             if name.endswith("png"):
-                result += urlparse.urljoin(root, name)
-    return result
+                output += "<a href=\"{0}\">{1}</a>".format(urlparse.urljoin(root, name), name)
+    output += "</td>\n"
+    f.write(output)
 
-def plugin_load_calling_log(testcase):
-    """Calling Log"""
-    pass
+def plugin_load_calling_log(testcase, f):
+    """CallingLog"""
+    output = "<td>\n"
+    for root, dir, files in os.walk(testcase):
+        for name in files:
+            if name.endswith("png"):
+                pass
+    output += "</td>\n"
+    f.write(output)
 
-def plugin_load_error_summary_log(testcase):
-    """Error Summary"""
-    pass
+def plugin_load_error_summary_log(testcase,f):
+    """ErrorSummary"""
+    output = "<td>\n"
+    for root, dir, files in os.walk(testcase):
+        for name in files:
+            if name.endswith("png"):
+                pass
+    output += "</td>\n"
+    f.write(output)
 
-def plugin_load_carrier_name_log(testcase):
-    """Carrier Nmae"""
-    pass
+def plugin_load_carrier_name_log(testcase, f):
+    """CarrierNmae"""
+    output = "<td>\n"
+    for root, dir, files in os.walk(testcase):
+        for name in files:
+            if name.endswith("png"):
+                pass
+    output += "</td>\n"
+    f.write(output)
+
+
+
 
 if __name__ == "__main__":
-    print plugin_load_carrier_name_log.__doc__
+
+
+    reporter_file_name = "enhanced_report.html"
+
+    with open(reporter_file_name, 'w') as f:
+        f.write(HEAD)
+        walk_suites("/home/guest/Automation/repo_main_L/tests/common-baseline/UI-automation/automation-scripts/result-062515-20-43-36", f)
+        f.write(FOOT)
+        f.close()
